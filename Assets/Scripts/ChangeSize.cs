476a416a0bea0878ce3a71c66764a5bd40b4c83e
jpernesz <1020894@student.sae.edu.au>
2020-07-07 16:55:26 +1000

    Added key components and scripts
    Added Lion, Door and door frame. Rotation objects and updated scripts including hover and rotation

--- Assets/Scripts/ChangeSize.cs
+++ Assets/Scripts/ChangeSize.cs
@@ -14,21 +14,44 @@ public class ChangeSize : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void MakeBig()
     {
 
-        transform.localScale = new Vector3(transform.localScale.x + 1, transform.localScale.y + 1, transform.localScale.z + 1);
+        transform.localScale = new UnityEngine.Vector3(transform.localScale.x + 1, transform.localScale.y + 1, transform.localScale.z + 1);
 
 
     }
 
     public void MakeSmall()
     {
-        transform.localScale = new Vector3(transform.localScale.x - 1, transform.localScale.y - 1, transform.localScale.z - 1);
+        transform.localScale = new UnityEngine.Vector3(transform.localScale.x - 1, transform.localScale.y - 1, transform.localScale.z - 1);
 
+
+    }
+
+    public void RotateRight()
+    {
+
+        transform.Rotate(0.0f, -rotateAmountDegrees, 0.0f, Space.Self);
+    }
+
+    public void RotateLeft()
+    {
+
+        transform.Rotate(0.0f, rotateAmountDegrees, 0.0f, Space.Self);
+    }
+
+    public void MoveLeft()
+    {
+        transform.Translate(UnityEngine.Vector3.left);
+    }
+
+    public void MoveRight()
+    {
+        transform.Translate(UnityEngine.Vector3.right);
 
     }
 }
